5c7785e279d56ad43eccf62d906824d0923ac425
marcos parajua <marcosparajua@gmail.com>
2024-03-10 21:39:07 +0100

    add js, append grid from DOM

--- main.js
+++ main.js
@@ -1,68 +1,96 @@
-class Bill {
-  #name;
-  #address;
-  #phone;
-  #document;
-  constructor(name, address, phone, document) {
-    this.#name = name;
-    this.#address = address;
-    this.#phone = phone;
-    this.#document = document;
-  }
-  getName(name) {
-    return this.#name;
-  }
+const rows = 50;
+const columns = 50;
 
-  getAddress(address) {
-    return this.#address;
+const initializeGrid = () => {
+  let grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < columns; j++) {
+      let alive;
+      if (Math.random() < 0.5) {
+        alive = 1;
+      } else {
+        alive = 0;
+      }
+      row.push(alive);
+    }
+    grid.push(row);
   }
-
-  getPhone(phone) {
-    return this.#phone;
-  }
-
-  getDocument(document) {
-    return this.#document;
-  }
-}
-
-class Client {
-  #name;
-  #address;
-  #phone;
-  #document;
-  constructor(name, address, phone, document) {
-    this.#name = name;
-    this.#address = address;
-    this.#phone = phone;
-    this.#document = document;
-  }
-  getName() {
-    return this.#name;
-  }
-
-  getAddress() {
-    return this.#address;
-  }
-
-  getPhone() {
-    return this.#phone;
-  }
-
-  getDocument() {
-    return this.#document;
-  }
-}
-
-const bill = new Bill('FakeInc LLC', '123 Main St', '555-1234', '123456789');
-console.log(bill.getName());
-console.log(bill.getAddress());
-console.log(bill.getPhone());
-console.log(bill.getDocument());
-
-const students = {
-  age: '',
-  name: '',
+  return grid;
 };
 
-Object.create();
+const updateGrid = (grid) => {
+  let newGrid = [];
+  for (let i = 0; i < rows; i++) {
+    let newRow = [];
+    for (let j = 0; j < columns; j++) {
+      const neighbors = countNeighbors(grid, i, j);
+      if (grid[i][j] === 1) {
+        if (neighbors === 2 || neighbors === 3) {
+          newRow.push(1);
+        } else {
+          newRow.push(0);
+        }
+      } else {
+        if (neighbors === 3) {
+          newRow.push(1);
+        } else {
+          newRow.push(0);
+        }
+      }
+    }
+    newGrid.push(newRow);
+  }
+  return newGrid;
+};
+
+function countNeighbors(grid, row, column) {
+  let count = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      const newRow = row + i;
+      const newColumn = column + j;
+
+      if (
+        newRow >= 0 &&
+        newRow < rows &&
+        newColumn >= 0 &&
+        newColumn < columns &&
+        !(i === 0 && j === 0) &&
+        grid[newRow][newColumn] === 1
+      ) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function renderGrid(grid) {
+  const gridContainer = document.querySelector('.grid');
+  gridContainer.innerHTML = '';
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const cell = document.createElement('div');
+      cell.className = 'grid__cell';
+      if (grid[i][j] === 1) {
+        cell.style.backgroundColor = 'black';
+      } else {
+        cell.style.backgroundColor = 'white';
+      }
+      gridContainer.appendChild(cell);
+    }
+  }
+}
+
+function startGame() {
+  let grid = initializeGrid();
+  renderGrid(grid);
+
+  setInterval(() => {
+    grid = updateGrid(grid);
+    renderGrid(grid);
+  }, 1000);
+}
+
+startGame();
